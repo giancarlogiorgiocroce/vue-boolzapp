d30012eb67614c15b0039f37768e94426e3dca93
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-05 12:47:09 +0200

    Setup iniziale

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Note:
+non è importante per ora che tutti gli elementi grafici siano presenti, l’importante che la struttura sia come da specifiche
+l’elenco degli utenti deve essere generato con un v-for e stampare almeno il nome e l’avatar (ultimo messaggio e data per ora possono essere fake)
+provate sia con tanti e pochi messaggi con messaggi lunghi o corti. La pagina non si deve mai spaccare
+*/
+
