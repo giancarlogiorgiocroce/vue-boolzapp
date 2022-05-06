ec16178415d0f9b83a7d0477c38701ff736f95cc
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-06 12:30:09 +0200

    Milestone 2 completato con prime prove sul visionare il last message del contatto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 /*
 Milestone 1
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-Note:
-non è importante per ora che tutti gli elementi grafici siano presenti, l’importante che la struttura sia come da specifiche
-l’elenco degli utenti deve essere generato con un v-for e stampare almeno il nome e l’avatar (ultimo messaggio e data per ora possono essere fake)
-provate sia con tanti e pochi messaggi con messaggi lunghi o corti. La pagina non si deve mai spaccare
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 */
 
 const app = new Vue({
     el: "#app",
     data: {
+        user:{
+            name: "Anna dall'Anna",
+            avatar: "_io"
+        },
+
         dataArray: [
             {
                 name: 'Michele',
@@ -176,9 +192,30 @@ const app = new Vue({
         ],
 
         currentContactIndex: 0,
-        
+
+        lastMessage: 0,
+   
     },
     methods:{
-
+        changeChat(i){
+            this.currentContactIndex = i;
+        },
+        isLastMessage(){
+            this.lastMessage = 1;
+        }
+        // isLastMessage(){
+        //     let rightData;
+        //     this.dataArray.forEach(data => {
+        //         rightData = data.messages;
+        //     });
+        //     console.log(rightData);
+        //     let rightMsg;
+        //     rightData.forEach((msg, i) => {
+        //         if(msg.status === received){
+        //             rightMsg = msg;
+        //         }
+        //         return msg;
+        //     })
+        // }
     }
 })
